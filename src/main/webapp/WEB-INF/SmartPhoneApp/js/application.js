a7899c065b04ff0277ba941c91aff0cac618c6cb
U28670 <U28670@B084DCOK.ustr.com>
2014-02-15 18:46:56 +0530

    ui flow correction smartphone app- fixed till the candidate list population

--- src/main/webapp/WEB-INF/SmartPhoneApp/js/application.js
+++ src/main/webapp/WEB-INF/SmartPhoneApp/js/application.js
@@ -1,3 +1,9 @@
+_elctionList = "";
+_eleSelected = "";
+_votingPin = "";
+_candidateList = "";
+_candSelected = "";
+
 $(document).ready(function(){
 	//alert("page loaded");
 	hideAll();
@@ -6,23 +12,23 @@ $(document).ready(function(){
 		eid=$('#in_eleid').val();
 		pin=$('#in_pin').val();
 		if(eid==""){
-			alert("eid is null");
-			$(".errortext").html("election id is empty");
+			alert("eid is null"); 		$(".errortext").html("election id is empty");
 		}
 		if(pin==""){
-			alert("pin is null");
-			$(".errortext").html("pin is empty");
+			alert("pin is null");		$(".errortext").html("pin is empty");
 		}
 		if(eid==""&&pin==""){
 			$(".errortext").html("information missing");
 		}
 		else{
+			_votingPin = pin;
 			manageLogin(eid,pin);
 		}
 	});
 	
 	$("#btn_elelist").click(function(){
-		showContent('#ContentCandidateList');
+		manageElectionSelection();
+	
 	});
 	$("#btn_candlist").click(function(){
 		showContent('#ContentOverview');
@@ -43,6 +49,53 @@ $(document).ready(function(){
   });
 
 
+function manageElectionSelection()
+{
+	var index = $('input[name=electionSelected]:checked').val();
+	_eleSelected = _elctionList[index];
+
+	obj = {"votingPIN":_votingPin,"electionId":_eleSelected.electId ,"unitElectionId":_eleSelected.unitEleId }
+	jsonString =JSON.stringify(obj);
+	
+	$.ajax({
+	    type: "POST",
+	    url: 'http://localhost:8080/mvote/vote/getCandidates',
+	    contentType: "application/json; charset=utf-8",
+	    dataType: "json",
+	    data: jsonString,
+	    success: function(response) {
+	    	populateCandidateList(response);
+	    }
+	});	
+}
+
+
+function populateCandidateList(response){
+	str = ""; 
+	if(response.customMessage == "SUCCESS")
+	{
+		_candidateList = response.candidateList;
+		if(_candidateList.length == 0)
+		{
+			str += "<p> No Candidates available for the user !</p>";
+		}else{
+			for(i=0; i< _candidateList.length; i++){
+				
+				str += 	"<input type='radio' name='canidateSelected' " ;
+				str += 	"value='"+ _candidateList[i].candId +"' checked='checked' /> " ;
+				str +=	 _candidateList[i].candName + ":" + _candidateList[i].candBio + "<br>" ;
+					
+			}
+		}
+	}else{
+		str += "<p>"+ response.customMessage +"</p>";
+	}
+	
+	showContent('#ContentCandidateList');
+	$("#fieldCandidatesList").html(str);
+}
+
+
 function manageLogin(eid, pin){
 	obj = {"eElectionId":eid, "votingPIN":pin  }
 	jsonString =JSON.stringify(obj);
@@ -54,20 +107,35 @@ function manageLogin(eid, pin){
 	    dataType: "json",
 	    data: jsonString,
 	    success: function(response) {
-	    	
-	    	//review
 	    	populateElectionList(response);
-	    	
 	    }
 	});	
-	
-	//review
-	//showContent('#ContentElectionList');
 }
 
 
-
-
+function populateElectionList(response){
+	str = "";
+	if(response.customMessage == "SUCCESS")
+	{
+		_elctionList = response.electionList;
+		if(_elctionList.length == 0)
+		{
+			str += "<p> No elections available for the user !</p>";
+		}else{
+			for(i=0; i<_elctionList.length; i++){
+				str += 	"<input type='radio' name='electionSelected' " ;
+				str += 	"value='"+ i +"' checked='checked' /> " ;
+				str +=	_elctionList[i].electTitle ;
+			}
+		}
+	}else{
+		str += "<p>"+ response.customMessage +"</p>";
+	}
+	
+	showContent('#ContentElectionList');
+	$("#fieldElectionList").html(str);
+	
+}
  
 function hideAll(){
 	 $("#ContentLogin").hide();
